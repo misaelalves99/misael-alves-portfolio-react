c7a6181d1388f7b8d02ec88ef87a9f2b93cb0889
Misael Alves <112561288+misaelalves99@users.noreply.github.com>
2025-01-03 11:05:42 -0300

    Header atualizado

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,6 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
-
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleMenu = () => {
@@ -13,17 +12,15 @@ export default function Header() {
 
     return (
         <>
-            <section className='header'>
-                <div className='container-header'>
-                    <div className='logo'>
-                        <Link to="/"><img src={Logo} alt={Logo} /></Link>
+            <header className="header">
+                <div className="container-header">
+                    <div className="logo">
+                        <Link to="/"><img src={Logo} alt="Logo" /></Link>
                     </div>
 
-                    <nav className='navbar'>
-                        <button className="menu-button" onClick={toggleMenu}>
-                            ☰
-                        </button>
-                        <ul className={`menu-list ${isOpen ? 'open' : ''}`}>
+                    <nav className="navbar">
+                        {/* Desktop Menu */}
+                        <ul className="desktop-menu">
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="/sobre">Sobre</Link></li>
                             <li><Link to="/habilidades">Habilidades</Link></li>
@@ -31,9 +28,28 @@ export default function Header() {
                             <li><Link to="/projetos">Projetos</Link></li>
                             <li><Link to="/contato">Contato</Link></li>
                         </ul>
+
+                        {/* Mobile Hamburger Menu */}
+                        <button className={`menu-button ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                            <div className="bar"></div>
+                            <div className="bar"></div>
+                            <div className="bar"></div>
+                        </button>
+
+                        {/* Fullscreen Mobile Menu */}
+                        <div className={`fullscreen-menu ${isOpen ? 'open' : ''}`}>
+                            <ul className="menu-list">
+                                <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
+                                <li><Link to="/sobre" onClick={toggleMenu}>Sobre</Link></li>
+                                <li><Link to="/habilidades" onClick={toggleMenu}>Habilidades</Link></li>
+                                <li><Link to="/experiencia" onClick={toggleMenu}>Experiência</Link></li>
+                                <li><Link to="/projetos" onClick={toggleMenu}>Projetos</Link></li>
+                                <li><Link to="/contato" onClick={toggleMenu}>Contato</Link></li>
+                            </ul>
+                        </div>
                     </nav>
                 </div>
-            </section>
+            </header>
         </>
     );
 }
